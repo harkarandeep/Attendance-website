d3ee5889258d083aca60d3c56330cdab147d85fc
Harkarandeep Singh Sidhu <harkarandeepsidhu@gmail.com>
2021-05-28 11:15:44 +0530

    modified

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,23 +1,32 @@
-const request=require('request')
-const forecast=((longitude,latitude,callback)=>{
-    const url='http://api.weatherstack.com/current?access_key=c01a8fb4985f143f82a79f61c00122d7&query=' + latitude+','+longitude+'&units=m'
-    request({url,json:true},(error,{body})=>{
-        if(error)
-        {
-            callback('Unable to connect to weather',undefined)
-        }
-        else if(body.error)
-        {
-            callback('Unable to find the location',undefined)
-        }
-        else{
-            callback(undefined,{
-                latitude:body.location.lat,
-                longitude:body.location.lon,
-                forecast:body.current.weather_descriptions[0]+'. It is currently '+body.current.temperature+' degrees out. It feels like '+body.current.feelslike+' degrees out.'
-            })
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c01a8fb4985f143f82a79f61c00122d7&query='+ encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)+'&units=m'
+    request({ url, json:true }, (error,{ body }) => {
+        if(error){
+            callback('Unable to connect to the internet.', undefined)
+        }else if(body.error){
+            callback('Unable to find the location.', undefined)
+        }else{
+            callback(undefined,('It is '+body.current.weather_descriptions+' currently. And temperature is '+ body.current.temperature+'. There is '+body.current.precip+'% chance of rain.'))
         }
     })
+}
 
-})
-module.exports=forecast
+module.exports = forecast
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
