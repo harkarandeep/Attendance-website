34840a25a0e911cab6056719dc87b44d36d756d8
Harkarandeep Singh Sidhu <harkarandeepsidhu@gmail.com>
2021-06-10 22:34:37 +0530

    Add attendace details

--- src/app.js
+++ src/app.js
@@ -1,125 +1,68 @@
 const path=require('path')
 const express=require('express')
 const hbs=require('hbs')
-const geocode=require('./utils/geocode')
-const forecast=require('./utils/forecast')
 
 const app=express()
 
 const port = process.env.PORT || 3000
 
-// Define paths for express config
+
 const dirpath = path.join(__dirname,'../public')
 const viewspath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
 
-//Ssetup handlebars engine and views location
+
 app.set('view engine','hbs')
-app.set('views',viewspath)      //Handle bars declaration
+app.set('views',viewspath)      
 hbs.registerPartials(partialsPath)
 
-// setup static directory to server
+
 app.use(express.static(dirpath))
 
-app.get('',(req,res)=>{      //It is used to use  the handlebars
-    res.render('index',{      // index is the view name
-        title:'Weather App',
-        name:'Harkarandeep Singh Sidhu'
+app.get('',(req,res)=>{      
+    res.render('index',{     
+        title:'Attendance App',
+        name:' Harkarandeep Singh Sidhu'
     })
 })
 
-app.get('/about',(req,res)=>{
-    res.render('about',{
-        title:'About me',
-        name:'harkarandeep Singh SIdhu'
+app.get('/absentDays',(req,res)=>{
+    var abc=9
+    res.render('absentDays',{
+        title:'Check number of days you were absent ',
+        name:' Harkarandeep Singh Sidhu',
+        absentDays: abc
     })
 })
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Hi this the help page',
-        name:'Harkarandeep Singh Sidhu'
+app.get('/absentDetails',(req,res)=>{
+    var der=['1/6/2021','2/6/2021','3/6/2021','4/6/2021']
+    res.render('absentDetails',{
+        title:'here are your Absent details',
+        name:' Harkarandeep Singh Sidhu',
+        absentDetails: der
     })
 })
 
-// app.get('/help',(req,res)=>{            //res=response  and req=request
-//     res.send([{
-//         name:'Harkarandeep'},{                  // array and json format
-//             name:'Aman'
-//     }])
-// })
-
-
-// app.get('/about',(req,res)=>{
-//     res.send('<title>Welcome to the about page</title><h1> welcome to about</h1>')    //handler  HTML format
-// })
-
-app.get('/weather',(req,res)=>{
-    if(!req.query.address){
-        return res.send({
-            error:'You must provide an address'
-        })
-    }
-    geocode(req.query.address, (error, { latitude, longitude, location}={}) =>{     //we have used ={} only to remove the error that will occur if we use ! on the site
-        if(error) {
-            return res.send({
-                error:error
-            })
-        }
-        forecast(latitude,longitude,(error,forecastData)=>{
-            if(error){
-                res.send({
-                    error:error 
-                })
-            }
-            res.send({
-                forecast: forecastData,
-                location,
-                address:req.query.address
-            })
-        })
+app.get('/leaveBalance',(req,res)=>{
+    var erd=['CL/Contingency Leave - 6 - 31/12/2021','Optional Holiday - 3 - 31/12/2021','Special Privilege Leave - 10 - 31/12/2021']
+    res.render('leaveBalance',{
+        title:'Check your leaveBalance Here',
+        name:' Harkarandeep',
+        leaveBalance: erd
     })
 })
 
-
-
-app.get('/products',(req,res)=>{
-
-    if(!req.query.search){
-        return res.send({                         // In this we used return just to stop the code from ruuning after the if statement
-            error:'You must provide a search item'
-        })
-    }
-    res.send({
-        products:[]
+app.get('/holidayCalander',(req,res)=>{   
+    var hyt=['15th August / Sunday - Independence Day ','10th September / Friday - Ganesh Chaturthi ','2nd October / Saturday - Gandhi Jayanti']
+    res.render('holidayCalander',{
+        title:'Welcome to Holiday section',
+        name:' Harkarandeep',
+        holidayCalander: hyt
     })
 })
 
-app.get('/help/*',(req,res)=>{
-    res.render('error',{
-        title:'404',
-        name:'Harkarandeep',
-        errorMessage:'Help Article not found'
-    })
-})
-
-app.get('*',(req,res)=>{         // the * sign is used to show th emessage for alll those sites which are not declared
-    res.render('error',{
-        title:'404',
-        name:'Harkarandeep',
-        errorMessage:'Page not found'
-    })
-})
-//app.com
-//app.com/help
-//app.com/about
-
-app.listen(port, ()=>{                          // to open th esite we need to use (localhost:3000)
-    console.log('server is on port 3000' + port)       //listen is used to start the server
+app.listen(port, ()=>{                          
+    console.log('server is on port ' + port)       
 })
        
-
-
-// app.get('',(req,res)=>{                //res=response  and req=request
-//     res.send('<h1>Hello express!</h1>')         
-// })
